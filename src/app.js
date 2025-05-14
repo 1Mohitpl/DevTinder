29313079da7ff7f4202978b901546a80e3dee27f
Mohit kumar paul <mohit1paul@gmail.com>
2025-05-14 22:37:49 +0530

    delete and get the user by age

--- src/app.js
+++ src/app.js
@@ -8,25 +8,76 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/sign-up", async (req, res) => {
-   
-  try{
-    // creating a new instance of the user model 
-    const  user = new User(req.body);  // data dynamic
-   
+  // creating a new instance of the user model
+  const user = new User(req.body); // data dynamic
 
-    await user.save() ;
-   res.send("user added successfully");
-
-  } catch(err){
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
     res.status(400).send(`eror message ${err.message}`);
   }
-  
+});
 
 
-  
-   
+
+// get the user by emailId
+app.get("/email", async (req, res) => {
+  try{
+   const person = await User.find({email : req.body.email});
+   if(person.length === 0){
+       return res.status(404).send("User not found");
+   } else {
+       return res.json(person)  
+   }
+    
+  } catch (err){
+     return  res.status(400).send(`message : ${err.message}`);
+  }
+     
 
 })
+
+// Get the user by age
+
+app.get("/age", async (req, res) => {
+     
+  const userbyAge = req.body.age;
+  try{
+    const user = await User.find({age : userbyAge});
+    if(userbyAge >=18){
+      return res.send(user);
+    } else {
+       res.status(404).send("User not found");
+    }
+  } catch(err){
+     res.status(400).send(`message : ${err}`);
+  }
+
+})
+
+// delete the user by userid
+
+app.delete("/delete", async (req, res) => {
+    const userId = req.body.userId;
+    try{
+       const user = await User.findByIdAndDelete(userId);
+       res.send("user delete successfully");
+    } catch (err) {
+        res.status(400).send(`message : ${err}`);
+    }
+})
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUser = await User.find();
+    return res.json(allUser);
+  } catch (eror) {
+    res.status(500).json({ error: "Unable to retrieve data" });
+  }
+});
+
+
 
 
 
@@ -41,5 +92,4 @@ connectDB()
   })
   .catch((err) => {
     console.log(`${err}`);
-
-});
+  });
